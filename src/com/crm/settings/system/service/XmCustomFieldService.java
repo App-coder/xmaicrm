d4b6203ab44002ec10a79805b04c9bc56c912852
mxzjzj <mxzjzj@gmail.com>
2012-08-23 19:24:41 +0800

    Signed-off-by: mxzjzj <mxzjzj@gmail.com>

--- src/com/crm/settings/system/service/XmCustomFieldService.java
+++ src/com/crm/settings/system/service/XmCustomFieldService.java
@@ -10,4 +10,12 @@ public interface XmCustomFieldService {
 	List<XmField> getFieldsByTabid(int tabid,int start,int rows);
 	
 	int insertCustomField(String queryParams);
+	
+	int getSequence(String table);
+	
+	int addCfField(String tablename, String cfField);
+	
+	String getTableNameByTabid(int tabid);
+	
+	int insert(XmField record,String queryParams);
 }
